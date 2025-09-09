fa044e3ff2f78b09f0f6eb4f6bfcbc3f8e7fffe5
Иванов Валерий Михайлович <v.ivanov@spbexchange.ru>
2025-09-09 15:11:26 +0300

    Updated sql query

--- CPMSender/Services/CpmRepo.cs
+++ CPMSender/Services/CpmRepo.cs
@@ -12,7 +12,7 @@ public interface ICpmRepo
 public class CpmRepo(PosttradeDbContext dbContext, IOptions<BotConfig> conf) : ICpmRepo
 {
     private string BuildQuery() =>
-        $"select trade_time, price, amount from \"Indiquote\" where instrument_instrument_id in ({conf.Value.InstrumentId}) order by  trade_time desc limit 1;"
+        $"select trade_time, price, amount from \"Indiquote\" where instrument_instrument_id in ({conf.Value.InstrumentId}) order by  trade_time desc limit 1"
             .ToString();
 
     public async Task<CurrentPriceOfMarket?> GetLatestCpmAsync(CancellationToken cancellationToken = default) =>
